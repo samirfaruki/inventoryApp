e767470a1fd05921bd0b4228ed164fb81cc1b2ec
samirfaruki <samir.faruki@softobiz.com>
2022-11-08 12:19:07 +0530

    Database Created

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const genreSchema = new Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+});
+
+genreSchema.virtual("url").get(function () {
+  return `/catalog/genre/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Genre", genreSchema);
